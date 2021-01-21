4b736c9826285c10cd24e75e129695f26e71a5b6
agu <agu@pop-os.localdomain>
2021-01-21 15:36:28 +0100

    var a, c, Xerase

--- js/app.js
+++ js/app.js
@@ -1,64 +1,79 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('js dziala');
 
-    // C
-    let erase= document.querySelector('.erase');
-    erase.addEventListener('click', function() {
-        console.log('C');
-        document.querySelector('.result').value= "";
-    });
-    
-    //(.onclick, petla)liczby let a, wyswietl
-    let numbers= document.getElementsByClassName('numbers'); //dlaczego querySelectorAll nie dziala??
-    for(let i = 0; i < numbers.length; i++) {
-        numbers[i].onclick = function(e) {
-            document.querySelector('.result').value = numbers[i].innerHTML;
-            let a= numbers[i].innerHTML;
-            console.log(a);
+let numbers = document.querySelectorAll('.numbers');
+let result = document.querySelector('.result');
+let a= '';
+let b= null;
+let c= '';
 
-             //(.onclick, petla)operations, let b, wyswietl
-            let operations= document.getElementsByClassName('operations'); //dlaczego querySelectorAll nie dziala??
-            for(let j = 0; j < operations.length; j++) {
-                operations[j].onclick = function(f) {
-                    document.querySelector('.result').value = operations[j].innerHTML;
-                    let b= operations[j].innerHTML;
-                    console.log(b);
-
-                    //(.onclick, petla)liczby let c, wyswietl
-                    let numbers2= document.getElementsByClassName('numbers'); //dlaczego querySelectorAll nie dziala??
-                    for(let k = 0; k < numbers2.length; k++) {
-                        numbers2[k].onclick = function(g) {
-                            document.querySelector('.result').value = numbers2[k].innerHTML;
-                            let c= numbers2[k].innerHTML;
-                            console.log(c);
-
-                            //dzielenie przez 0
-                            if (b == '/' && c == 0) {
-                                alert('dividing by 0...');
-                                document.querySelector('.result').value= "";
-                            }   else {
-
-                                    //wynik
-                                    let equals= document.querySelector('.equals');
-                                    equals.addEventListener('click', function() {
-                                        console.log('=');
-                                        document.querySelector('.result').value= eval(a + b + c);
-                                        console.log(eval(a + b + c));
-
-                                        //C
-                                        let erase= document.querySelector('.erase');
-                                        erase.addEventListener('click', function() {
-                                            console.log('C');
-                                            document.querySelector('.result').value= "";
-                                        })
-                                    })
-                                }
-                        }
-                    }        
-                }
-            }
-        }
+//zmienna a i c
+function liczby(cyfra) {
+    if ( b === null && c === '') {
+        console.log(cyfra);
+        a+= cyfra;
+        result.value = a;
+        console.log('a:', a, 'b:', b, 'c:', c);
+        return;
+    // } else if (function equals()) {
+    //     a+= cyfra;
+    //     result.value = a;
+    //     console.log('a:', a, 'b:', b, 'c:', c);
+    //     b = null;
+    //     c = '';
+    //     return;
+    } else {
+        console.log(cyfra);
+        c+= cyfra;
+        result.value = c;
+        console.log('a:', a, 'b:', b, 'c:', c);
+        return;
     }
+}
 
-});
+//zmienna b
+function sum() {
+    result.value = '+';
+    b = '+';
+    console.log('a:', a, 'b:', b, 'c:', c);
+}
+
+function sub() {
+    result.value = '-';
+    b = '-';
+    console.log('a:', a, 'b:', b, 'c:', c);
+}
+
+function multi() {
+    result.value = '*';
+    b = '*';
+    console.log('a:', a, 'b:', b, 'c:', c);
+}
+
+function div() {
+    result.value = '/';
+    b = '/';
+    console.log('a:', a, 'b:', b, 'c:', c);
+}
+
+//wynik
+function equals() {
+    console.log('=');
+    result.value = '= ' + eval( a + b + c);
+    console.log(eval(a+b+c) ,'a:', a, 'b:', b, 'c:', c);
+}
+
+//C
+function erase() {
+    result.value= '';
+    a = '';
+    b = null;
+    c = '';
+    console.log('a:', a, 'b:', b, 'c:', c);
+}
+
+//Xerase
+function Xerase() {
+    let l= result.value.slice(0, -1);
+    console.log(l);
+    result.value=l;
+}
